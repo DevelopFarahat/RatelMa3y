95fda6fe1815c0939712f2cff7e5cbfd9324fec2
Mohamed Farhat <mohamed.farhat.15155@commerce.helwan.edu.eg>
2022-09-07 09:45:21 +0200

    adding messages to adminPanel but right now it is not completed till the backend finished it

--- src/components/admin_panel/Admin_Panel.js
+++ src/components/admin_panel/Admin_Panel.js
@@ -10,22 +10,23 @@ const AdminPanel = () => {
         SystemUsersActive: true,
         addPostActive: false,
         studentActive: false,
-        instructorActive: false
+        instructorActive: false,
+        messagesActive:false
 
     });
 
     const activateLink = (event) => {
-        event.target.id === 'SystemUsersActive' ? setActiveLinks({ SystemUsersActive: true, addPostActive: false, studentActive: false, instructorActive: false }) : event.target.id === 'addPostActive' ? setActiveLinks({ SystemUsersActive: false, addPostActive: true, studentActive: false, instructorActive: false }) : event.target.id === 'studentActive' ? setActiveLinks({ SystemUsersActive: false, addPostActive: false, studentActive: true, instructorActive: false }) : setActiveLinks({ SystemUsersActive: false, addPostActive: false, studentActive: false, instructorActive: true })
+        event.target.id === 'SystemUsersActive' ? setActiveLinks({ SystemUsersActive: true, addPostActive: false, studentActive: false, instructorActive: false,messagesActive:false }) : event.target.id === 'addPostActive' ? setActiveLinks({ SystemUsersActive: false, addPostActive: true, studentActive: false, instructorActive: false,messagesActive:false }) : event.target.id === 'studentActive' ? setActiveLinks({ SystemUsersActive: false, addPostActive: false, studentActive: true, instructorActive: false,messagesActive:false }): event.target.id === 'messagesActive'?setActiveLinks({ SystemUsersActive: false, addPostActive: false, studentActive: false, instructorActive: false,messagesActive:true })  : setActiveLinks({ SystemUsersActive: false, addPostActive: false, studentActive: false, instructorActive: true,messagesActive:false })
     }
 
     return (
         <>
             <div className={AdminPanelStyles['admin-panel-navigation']}>
                 <Link className={`${activeLinks.SystemUsersActive? AdminPanelStyles['active-tap']:AdminPanelStyles['navigation-links']}`} to={"/adminPanel/systemUsers"} id="SystemUsersActive" onClick={(event)=>activateLink(event)}>{t("systemUsers")}</Link>
-            
                 <Link className={`${activeLinks.addPostActive? AdminPanelStyles['active-tap']:AdminPanelStyles['navigation-links']}`} to={"/adminPanel/addPost"}  id="addPostActive" onClick={(event)=>activateLink(event)}>{t("adminpanel_addpost")}</Link>
                 <Link className={`${activeLinks.studentActive? AdminPanelStyles['active-tap']:AdminPanelStyles['navigation-links']}`}  to={"/adminPanel/students"}   id="studentActive" onClick={(event)=>activateLink(event)}>{t("adminpanel_students")}</Link>
                 <Link className={`${activeLinks.instructorActive? AdminPanelStyles['active-tap']:AdminPanelStyles['navigation-links']}`} to={"/adminPanel/instructors"}   id="instructorActive" onClick={(event)=>activateLink(event)}>{t("instructor")}</Link> 
+                <Link className={`${activeLinks.messagesActive? AdminPanelStyles['active-tap']:AdminPanelStyles['navigation-links']}`} to={"/adminPanel/messages"}   id="messagesActive" onClick={(event)=>activateLink(event)}>Messages</Link>
             </div>
             <div className={AdminPanelStyles['main-container']}>
                 <Outlet />
